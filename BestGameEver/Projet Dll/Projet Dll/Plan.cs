b298fcd292f37412ff3f1f17b61898c2e3a9338b
Aksii <eyouri.0@gmail.com>
2014-01-24 06:37:54 -0400

    Update

--- BestGameEver/Projet Dll/Projet Dll/Plan.cs
+++ BestGameEver/Projet Dll/Projet Dll/Plan.cs
@@ -1,77 +1,157 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using Microsoft.Xna.Framework.Graphics;
+//using Microsoft.Xna.Framework;
+
+//namespace TOOLS
+//{
+//   public abstract class Plan : PrimitiveDeBaseAnimée
+//   {
+//      protected Vector3 Origine { get; private set; }  //Le coin inférieur gauche du plan en tenant compte que la primitive est centrée au point (0,0,0)
+//      Vector2 Delta { get; set; } // un vecteur contenant l'espacement entre deux colonnes (en X) et entre deux rangées (en Y)
+//      protected Vector3[,] PtsSommets { get; private set; } //un tableau contenant les positions des différents sommets du plan
+//      protected int NbColonnes { get; private set; } // Devinez...
+//      protected int NbRangées { get; private set; } // idem 
+//      protected int NbTrianglesParStrip { get; private set; } //...
+//      protected BasicEffect EffetDeBase { get; private set; } // 
+
+//      public Plan(Game jeu, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector2 étendue, Vector2 charpente, float intervalleMAJ)
+//         : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale, intervalleMAJ)
+//      {
+//          NbColonnes = (int)charpente.X;
+//          NbRangées = (int)charpente.Y;
+//          Delta = new Vector2(étendue.X / NbColonnes, étendue.Y / NbRangées);
+//          Origine = new Vector3(-étendue.X / 2, -étendue.Y / 2, 0);
+//      }
+
+//      public override void Initialize()
+//      {
+//         NbTrianglesParStrip = NbColonnes * 2;
+//         NbSommets = (NbTrianglesParStrip + 2) * NbRangées;
+//         PtsSommets = new Vector3[NbColonnes + 1, NbRangées + 1];
+//         CréerTableauSommets();
+//         CréerTableauPoints();
+//         base.Initialize();
+//      }
+
+//      protected abstract void CréerTableauSommets();
+
+//      protected override void LoadContent()
+//      {
+//         EffetDeBase = new BasicEffect(GraphicsDevice);
+//         InitialiserParamètresEffetDeBase();
+//         base.LoadContent();
+//      }
+
+//      protected abstract void InitialiserParamètresEffetDeBase();
+
+//      private void CréerTableauPoints()
+//      {
+//         for (int noColonne = 0; noColonne <= NbColonnes; noColonne++)
+//         {
+//            for (int noRangée = 0; noRangée <= NbRangées; noRangée++)
+//            {
+//               PtsSommets[noColonne, noRangée] = new Vector3(Origine.X + noColonne * Delta.X, Origine.Y + noRangée * Delta.Y, Origine.Z);
+//            }
+//         }
+//      }
+
+//      public override void Draw(GameTime gameTime)
+//      {
+//         EffetDeBase.World = GetMonde();
+//         EffetDeBase.View = CaméraJeu.Vue;
+//         EffetDeBase.Projection = CaméraJeu.Projection;
+//         foreach (EffectPass passeEffet in EffetDeBase.CurrentTechnique.Passes)
+//         {
+//            passeEffet.Apply();
+//            for (int i = 0; i < NbRangées; ++i)
+//            {
+//               DessinerTriangleStrip(i);
+//            }
+//         }
+//         base.Draw(gameTime);
+//      }
+
+//      protected abstract void DessinerTriangleStrip(int noStrip);
+//   }
+//}
+using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 
-namespace TOOLS
+namespace AtelierXNA
 {
-   public abstract class Plan : PrimitiveDeBaseAnimée
-   {
-      protected Vector3 Origine { get; private set; }  //Le coin inférieur gauche du plan en tenant compte que la primitive est centrée au point (0,0,0)
-      Vector2 Delta { get; set; } // un vecteur contenant l'espacement entre deux colonnes (en X) et entre deux rangées (en Y)
-      protected Vector3[,] PtsSommets { get; private set; } //un tableau contenant les positions des différents sommets du plan
-      protected int NbColonnes { get; private set; } // Devinez...
-      protected int NbRangées { get; private set; } // idem 
-      protected int NbTrianglesParStrip { get; private set; } //...
-      protected BasicEffect EffetDeBase { get; private set; } // 
+    public abstract class Plan : PrimitiveDeBase
+    {
+        const int NB_TRIANGLES = 2;
+        protected Vector3 Origine { get; private set; }  //Le coin inférieur gauche du plan en tenant compte que la primitive est centrée au point (0,0,0)
+        Vector2 Delta { get; set; } // un vecteur contenant l'espacement entre deux colonnes (en X) et entre deux rangées (en Y)
+        protected Vector3[,] TableauPoints { get; private set; } //un tableau contenant les positions des différents sommets du plan
+        protected int NbColonnes { get; private set; } // Devinez...
+        protected int NbRangées { get; private set; } // idem 
+        protected int NbTrianglesParStrip { get; private set; } //...
+        protected BasicEffect EffetDeBase { get; private set; } // 
 
-      public Plan(Game jeu, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector2 étendue, Vector2 charpente, float intervalleMAJ)
-         : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale, intervalleMAJ)
-      {
-          NbColonnes = (int)charpente.X;
-          NbRangées = (int)charpente.Y;
-          Delta = new Vector2(étendue.X / NbColonnes, étendue.Y / NbRangées);
-          Origine = new Vector3(-étendue.X / 2, -étendue.Y / 2, 0);
-      }
+        public Plan(Game jeu, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector2 étendue, Vector2 charpente, float intervalleMAJ)
+            : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale)
+        {
+            NbColonnes = (int)charpente.X;
+            NbRangées = (int)charpente.Y;
+            Delta = étendue / charpente;
+            Origine = new Vector3(-étendue.X / 2, -étendue.Y / 2, 0);
+        }
 
-      public override void Initialize()
-      {
-         NbTrianglesParStrip = NbColonnes * 2;
-         NbSommets = (NbTrianglesParStrip + 2) * NbRangées;
-         PtsSommets = new Vector3[NbColonnes + 1, NbRangées + 1];
-         CréerTableauSommets();
-         CréerTableauPoints();
-         base.Initialize();
-      }
+        public override void Initialize()
+        {
+            NbTrianglesParStrip = NbColonnes * 2;
+            NbSommets = (NbTrianglesParStrip + 2) * NbRangées;
+            TableauPoints = new Vector3[NbColonnes + 1, NbRangées + 1];
+            CréerTableauSommets();
+            CréerTableauPoints();
+            base.Initialize();
+        }
 
-      protected abstract void CréerTableauSommets();
+        protected abstract void CréerTableauSommets();
 
-      protected override void LoadContent()
-      {
-         EffetDeBase = new BasicEffect(GraphicsDevice);
-         InitialiserParamètresEffetDeBase();
-         base.LoadContent();
-      }
+        protected override void LoadContent()
+        {
+            EffetDeBase = new BasicEffect(GraphicsDevice);
+            InitialiserParamètresEffetDeBase();
+            base.LoadContent();
+        }
 
-      protected abstract void InitialiserParamètresEffetDeBase();
+        protected abstract void InitialiserParamètresEffetDeBase();
 
-      private void CréerTableauPoints()
-      {
-         for (int noColonne = 0; noColonne <= NbColonnes; noColonne++)
-         {
-            for (int noRangée = 0; noRangée <= NbRangées; noRangée++)
+        private void CréerTableauPoints()
+        {
+            for (int j = 0; j <= NbRangées; j++)
             {
-               PtsSommets[noColonne, noRangée] = new Vector3(Origine.X + noColonne * Delta.X, Origine.Y + noRangée * Delta.Y, Origine.Z);
+                for (int i = 0; i <= NbColonnes; i++)
+                {
+                    TableauPoints[i, j] = new Vector3(Origine.X + (Delta.X * i), Origine.Y + (Delta.Y * j), Origine.Z);
+                }
             }
-         }
-      }
+        }
 
-      public override void Draw(GameTime gameTime)
-      {
-         EffetDeBase.World = GetMonde();
-         EffetDeBase.View = CaméraJeu.Vue;
-         EffetDeBase.Projection = CaméraJeu.Projection;
-         foreach (EffectPass passeEffet in EffetDeBase.CurrentTechnique.Passes)
-         {
-            passeEffet.Apply();
-            for (int i = 0; i < NbRangées; ++i)
+        public override void Draw(GameTime gameTime)
+        {
+            EffetDeBase.World = GetMonde();
+            EffetDeBase.View = CaméraJeu.Vue;
+            EffetDeBase.Projection = CaméraJeu.Projection;
+            foreach (EffectPass passeEffet in EffetDeBase.CurrentTechnique.Passes)
             {
-               DessinerTriangleStrip(i);
+                passeEffet.Apply();
+                for (int i = 0; i < NbRangées; i++)
+                {
+                    DessinerTriangleStrip(i);
+                }
+                // Ici, il devrait y avoir une boucle qui provoque le dessin de chaque TriangleStrip du plan
+                // Le dessin d'un TriangleStrip en particulier devrait se faire en appellan la méthode DessinerTriangleStrip()
             }
-         }
-         base.Draw(gameTime);
-      }
+            base.Draw(gameTime);
+        }
 
-      protected abstract void DessinerTriangleStrip(int noStrip);
-   }
+        protected abstract void DessinerTriangleStrip(int noStrip);
+    }
 }
--- BestGameEver/Projet Dll/Projet Dll/PlanTexturé.cs
+++ BestGameEver/Projet Dll/Projet Dll/PlanTexturé.cs
@@ -1,88 +1,162 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+//using Microsoft.Xna.Framework.Graphics;
+//using Microsoft.Xna.Framework;
+
+//namespace TOOLS
+//{
+//   class PlanTexturé : Plan
+//   {
+//      RessourcesManager<Texture2D> GestionnaireDeTextures;
+//      Texture2D TexturePlan;
+//      protected VertexPositionTexture[] Sommets { get; set; }
+//      Vector2[,] PtsTexture { get; set; }
+//      string NomTexturePlan { get; set; }
+//      BlendState GestionAlpha { get; set; }
+
+//      public PlanTexturé(Game jeu, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector2 étendue, Vector2 charpente, string nomTexturePlan, float intervalleMAJ)
+//         : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale, étendue, charpente, intervalleMAJ)
+//      {
+//         NomTexturePlan = nomTexturePlan;
+//      }
+
+//      protected override void LoadContent()
+//      {
+//         GestionnaireDeTextures = Game.Services.GetService(typeof(RessourcesManager<Texture2D>)) as RessourcesManager<Texture2D>;
+//         TexturePlan = GestionnaireDeTextures.Find(NomTexturePlan);
+//         base.LoadContent();
+//      }
+
+//      protected override void CréerTableauSommets()
+//      {
+//         PtsTexture = new Vector2[NbColonnes + 1, NbRangées + 1];
+//         CréerTableauPointsTexture();
+//         Sommets = new VertexPositionTexture[NbSommets];
+//      }
+
+//      protected override void InitialiserParamètresEffetDeBase()
+//      {
+//         EffetDeBase.TextureEnabled = true;
+//         EffetDeBase.Texture = TexturePlan;
+//         GestionAlpha = BlendState.AlphaBlend;
+//      }
+
+//      private void CréerTableauPointsTexture()
+//      {
+//         float positionX = 0;
+//         float deltaX = 1 / (float)NbColonnes;
+//         float deltaY = 1 / (float)NbRangées;
+
+//         for (int i = 0; i <= NbColonnes; i++)
+//         {
+//            float positionY = 1;
+//            for (int j = 0; j <= NbRangées; j++)
+//            {
+//               PtsTexture[i, j] = new Vector2(positionX, positionY);
+//               positionY -= deltaY;
+//            }
+//            positionX += deltaX;
+//         }
+//      }
+
+//      protected override void InitialiserSommets() // Est appelée par base.Initialize()
+//      {
+//         int noSommet = -1;
+//         for (int j = 0; j < NbRangées; ++j)
+//         {
+//            for (int i = 0; i <= NbColonnes; ++i)
+//            {
+//               Sommets[++noSommet] = new VertexPositionTexture(PtsSommets[i, j], PtsTexture[i, j]);
+//               Sommets[++noSommet] = new VertexPositionTexture(PtsSommets[i, j + 1], PtsTexture[i, j + 1]);
+//            }
+//         }
+//      }
+
+//      public override void Draw(GameTime gameTime)
+//      {
+//         BlendState blendState = GraphicsDevice.BlendState;
+//         GraphicsDevice.BlendState = GestionAlpha;
+//         base.Draw(gameTime);
+//         GraphicsDevice.BlendState = blendState;
+//      }
+
+//      protected override void DessinerTriangleStrip(int noStrip)
+//      {
+//         GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, Sommets, (NbTrianglesParStrip + 2) * noStrip, NbTrianglesParStrip);
+//      }
+//   }
+//}
+using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 
-namespace TOOLS
+namespace AtelierXNA
 {
-   class PlanTexturé : Plan
-   {
-      RessourcesManager<Texture2D> GestionnaireDeTextures;
-      Texture2D TexturePlan;
-      protected VertexPositionTexture[] Sommets { get; set; }
-      Vector2[,] PtsTexture { get; set; }
-      string NomTexturePlan { get; set; }
-      BlendState GestionAlpha { get; set; }
+    public class PlanTexturé : Plan
+    {
+        RessourcesManager<Texture2D> gestionnaireDeTextures;
+        Vector2[,] PtsTexture { get; set; }
+        protected VertexPositionTexture[] TableauSommets;
+        string NomTexturePlan { get; set; }
+        Texture2D TexturePlan;
+        BlendState GestionAlpha { get; set; }
 
-      public PlanTexturé(Game jeu, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector2 étendue, Vector2 charpente, string nomTexturePlan, float intervalleMAJ)
-         : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale, étendue, charpente, intervalleMAJ)
-      {
-         NomTexturePlan = nomTexturePlan;
-      }
-
-      protected override void LoadContent()
-      {
-         GestionnaireDeTextures = Game.Services.GetService(typeof(RessourcesManager<Texture2D>)) as RessourcesManager<Texture2D>;
-         TexturePlan = GestionnaireDeTextures.Find(NomTexturePlan);
-         base.LoadContent();
-      }
-
-      protected override void CréerTableauSommets()
-      {
-         PtsTexture = new Vector2[NbColonnes + 1, NbRangées + 1];
-         CréerTableauPointsTexture();
-         Sommets = new VertexPositionTexture[NbSommets];
-      }
-
-      protected override void InitialiserParamètresEffetDeBase()
-      {
-         EffetDeBase.TextureEnabled = true;
-         EffetDeBase.Texture = TexturePlan;
-         GestionAlpha = BlendState.AlphaBlend;
-      }
-
-      private void CréerTableauPointsTexture()
-      {
-         float positionX = 0;
-         float deltaX = 1 / (float)NbColonnes;
-         float deltaY = 1 / (float)NbRangées;
-
-         for (int i = 0; i <= NbColonnes; i++)
-         {
-            float positionY = 1;
+        public PlanTexturé(Game jeu, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector2 étendue, Vector2 charpente, string nomTexture, float intervalleMAJ)
+            : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale, étendue, charpente, intervalleMAJ)
+        {
+            NomTexturePlan = nomTexture;
+        }
+        protected override void LoadContent()
+        {
+            gestionnaireDeTextures = Game.Services.GetService(typeof(RessourcesManager<Texture2D>)) as RessourcesManager<Texture2D>;
+            TexturePlan = gestionnaireDeTextures.Find(NomTexturePlan);
+            base.LoadContent();
+        }
+        protected override void InitialiserSommets()
+        {
+            int k = 0;
+            for (int j = 0; j < NbRangées; ++j)
+            {
+                for (int i = 0; i <= NbColonnes; ++i)
+                {
+                    TableauSommets[k++] = new VertexPositionTexture(TableauPoints[i, j], PtsTexture[i, j]);
+                    TableauSommets[k++] = new VertexPositionTexture(TableauPoints[i, (j + 1)], PtsTexture[i, (j + 1)]);
+                }
+            }
+        }
+        protected override void CréerTableauSommets()
+        {
+            PtsTexture = new Vector2[NbColonnes + 1, NbRangées + 1];
+            TableauSommets = new VertexPositionTexture[NbSommets];
+            CréerTableauPointsTexture();
+        }
+        private void CréerTableauPointsTexture()
+        {
             for (int j = 0; j <= NbRangées; j++)
             {
-               PtsTexture[i, j] = new Vector2(positionX, positionY);
-               positionY -= deltaY;
+                for (int i = 0; i <= NbColonnes; i++)
+                {
+                    PtsTexture[i, j] = new Vector2((float)i / NbColonnes, ((float)NbRangées - j) / NbRangées);
+                }
             }
-            positionX += deltaX;
-         }
-      }
-
-      protected override void InitialiserSommets() // Est appelée par base.Initialize()
-      {
-         int noSommet = -1;
-         for (int j = 0; j < NbRangées; ++j)
-         {
-            for (int i = 0; i <= NbColonnes; ++i)
-            {
-               Sommets[++noSommet] = new VertexPositionTexture(PtsSommets[i, j], PtsTexture[i, j]);
-               Sommets[++noSommet] = new VertexPositionTexture(PtsSommets[i, j + 1], PtsTexture[i, j + 1]);
-            }
-         }
-      }
-
-      public override void Draw(GameTime gameTime)
-      {
-         BlendState blendState = GraphicsDevice.BlendState;
-         GraphicsDevice.BlendState = GestionAlpha;
-         base.Draw(gameTime);
-         GraphicsDevice.BlendState = blendState;
-      }
-
-      protected override void DessinerTriangleStrip(int noStrip)
-      {
-         GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, Sommets, (NbTrianglesParStrip + 2) * noStrip, NbTrianglesParStrip);
-      }
-   }
+        }
+        protected override void InitialiserParamètresEffetDeBase()
+        {
+            EffetDeBase.TextureEnabled = true;
+            EffetDeBase.Texture = TexturePlan;
+            GestionAlpha = BlendState.AlphaBlend;
+        }
+        protected override void DessinerTriangleStrip(int noStrip)
+        {
+            GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, TableauSommets, noStrip * (NbTrianglesParStrip + 2), NbTrianglesParStrip);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            BlendState oldBlendState = GraphicsDevice.BlendState;
+            GraphicsDevice.BlendState = GestionAlpha;
+            base.Draw(gameTime);
+            GraphicsDevice.BlendState = oldBlendState;
+        }
+    }
 }
