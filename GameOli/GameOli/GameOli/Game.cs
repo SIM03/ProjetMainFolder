a40c8cb53ac997c18ca16dc1350dd5f5cf6799bc
Trottier95 <trottier.sam@gmail.com>
2014-01-28 16:48:20 -0500

    Salle Complète
    Une version de la salle complète avec tous les murs et le plancher. La
    caméra est, cependant, toujours pas fluide

--- GameOli/GameOli/GameOli/Game.cs
+++ GameOli/GameOli/GameOli/Game.cs
@@ -14,12 +14,12 @@ namespace GAME
 {
     class Game : Microsoft.Xna.Framework.Game
     {
-        const float DIMENSION_X = 1024;
+        const float DIMENSION_X = 512;
         const float DIMENSION_Y = 200;
-        const float DIMENSION_Z = 2048;
+        const float DIMENSION_Z = 1024;
         Vector2 étenduePlan = new Vector2(DIMENSION_X,DIMENSION_Y);
         Vector2 étenduePlan1 = new Vector2(DIMENSION_X, DIMENSION_X + DIMENSION_Z);
-        Vector2 charpentePlan = new Vector2(16,9);
+        Vector2 charpentePlan = new Vector2(4,3);
 
         const float INTERVALLE_CALCUL_FPS = 1f;
         const float INTERVALLE_MAJ_STANDARD = 1f / 60f;
@@ -79,12 +79,19 @@ namespace GAME
             Components.Add(new PlanTexturé(this, 1f, new Vector3(0, -MathHelper.PiOver2, 0), new Vector3(DIMENSION_X / 2, (float)1.5 * DIMENSION_Y, DIMENSION_Z / 2), étenduePlan, charpentePlan, "Wall", INTERVALLE_MAJ_STANDARD));
             Components.Add(new PlanTexturé(this, 1f, new Vector3(0, -MathHelper.PiOver2, 0), new Vector3(DIMENSION_X / 2, (float)1.5 * DIMENSION_Y,0), étenduePlan, charpentePlan, "Wall", INTERVALLE_MAJ_STANDARD));
 
-            //Murs des fonds
-            Components.Add(new PlanTexturé(this, 1f, Vector3.Zero, new Vector3(0, DIMENSION_Y / 2, -DIMENSION_Z), étenduePlan, charpentePlan, "Wall", INTERVALLE_MAJ_STANDARD));
-            Components.Add(new PlanTexturé(this, 1f, new Vector3(0, -MathHelper.Pi, 0), new Vector3(0, DIMENSION_Y/ 2, DIMENSION_Z), étenduePlan, charpentePlan, "Wall", INTERVALLE_MAJ_STANDARD));
+            //Murs des fonds Bas
+            Components.Add(new PlanTexturé(this, 1f, Vector3.Zero, new Vector3(0, DIMENSION_Y / 2, 3 * -DIMENSION_Z / 4), étenduePlan, charpentePlan, "Wall", INTERVALLE_MAJ_STANDARD));
+            Components.Add(new PlanTexturé(this, 1f, new Vector3(0, -MathHelper.Pi, 0), new Vector3(0, DIMENSION_Y / 2,3 * DIMENSION_Z / 4), étenduePlan, charpentePlan, "Wall", INTERVALLE_MAJ_STANDARD));
+
+            //Murs des fonds Haut
+            Components.Add(new PlanTexturé(this, 1f, Vector3.Zero, new Vector3(0, (float)1.5 * DIMENSION_Y, 3 * -DIMENSION_Z / 4), étenduePlan, charpentePlan, "Wall", INTERVALLE_MAJ_STANDARD));
+            Components.Add(new PlanTexturé(this, 1f, new Vector3(0, -MathHelper.Pi, 0), new Vector3(0, (float)1.5 * DIMENSION_Y, 3 * DIMENSION_Z / 4), étenduePlan, charpentePlan, "Wall", INTERVALLE_MAJ_STANDARD));
 
             //Plafond
             Components.Add(new PlanTexturé(this, 1f, new Vector3(MathHelper.PiOver2, 0, 0), new Vector3(0, 2* DIMENSION_Y, 0), étenduePlan1, charpentePlan, "Roof", INTERVALLE_MAJ_STANDARD));
+
+            //Plancher
+            Components.Add(new PlanTexturé(this, 1f, new Vector3(-MathHelper.PiOver2, 0, 0), new Vector3(0,0, 0), étenduePlan1, charpentePlan, "Floor", INTERVALLE_MAJ_STANDARD));
 
             Services.AddService(typeof(RessourcesManager<SpriteFont>), GestionnaireDeFonts);
             Services.AddService(typeof(RessourcesManager<Texture2D>), GestionnaireDeTextures);
--- GameOli/Projet Dll/PlanTexturé.cs
+++ GameOli/Projet Dll/PlanTexturé.cs
@@ -5,84 +5,84 @@ using Microsoft.Xna.Framework;
 
 namespace TOOLS
 {
-   class PlanTexturé : Plan
-   {
-      RessourcesManager<Texture2D> GestionnaireDeTextures;
-      Texture2D TexturePlan;
-      protected VertexPositionTexture[] Sommets { get; set; }
-      Vector2[,] PtsTexture { get; set; }
-      string NomTexturePlan { get; set; }
-      BlendState GestionAlpha { get; set; }
+    public class PlanTexturé : Plan
+    {
+        RessourcesManager<Texture2D> GestionnaireDeTextures;
+        Texture2D TexturePlan;
+        protected VertexPositionTexture[] Sommets { get; set; }
+        Vector2[,] PtsTexture { get; set; }
+        string NomTexturePlan { get; set; }
+        BlendState GestionAlpha { get; set; }
 
-      public PlanTexturé(Game jeu, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector2 étendue, Vector2 charpente, string nomTexturePlan, float intervalleMAJ)
-         : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale, étendue, charpente, intervalleMAJ)
-      {
-         NomTexturePlan = nomTexturePlan;
-      }
+        public PlanTexturé(Game jeu, float homothétieInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, Vector2 étendue, Vector2 charpente, string nomTexturePlan, float intervalleMAJ)
+            : base(jeu, homothétieInitiale, rotationInitiale, positionInitiale, étendue, charpente, intervalleMAJ)
+        {
+            NomTexturePlan = nomTexturePlan;
+        }
 
-      protected override void LoadContent()
-      {
-         GestionnaireDeTextures = Game.Services.GetService(typeof(RessourcesManager<Texture2D>)) as RessourcesManager<Texture2D>;
-         TexturePlan = GestionnaireDeTextures.Find(NomTexturePlan);
-         base.LoadContent();
-      }
+        protected override void LoadContent()
+        {
+            GestionnaireDeTextures = Game.Services.GetService(typeof(RessourcesManager<Texture2D>)) as RessourcesManager<Texture2D>;
+            TexturePlan = GestionnaireDeTextures.Find(NomTexturePlan);
+            base.LoadContent();
+        }
 
-      protected override void CréerTableauSommets()
-      {
-         PtsTexture = new Vector2[NbColonnes + 1, NbRangées + 1];
-         CréerTableauPointsTexture();
-         Sommets = new VertexPositionTexture[NbSommets];
-      }
+        protected override void CréerTableauSommets()
+        {
+            PtsTexture = new Vector2[NbColonnes + 1, NbRangées + 1];
+            CréerTableauPointsTexture();
+            Sommets = new VertexPositionTexture[NbSommets];
+        }
 
-      protected override void InitialiserParamètresEffetDeBase()
-      {
-         EffetDeBase.TextureEnabled = true;
-         EffetDeBase.Texture = TexturePlan;
-         GestionAlpha = BlendState.AlphaBlend;
-      }
+        protected override void InitialiserParamètresEffetDeBase()
+        {
+            EffetDeBase.TextureEnabled = true;
+            EffetDeBase.Texture = TexturePlan;
+            GestionAlpha = BlendState.AlphaBlend;
+        }
 
-      private void CréerTableauPointsTexture()
-      {
-         float positionX = 0;
-         float deltaX = 1 / (float)NbColonnes;
-         float deltaY = 1 / (float)NbRangées;
+        private void CréerTableauPointsTexture()
+        {
+            float positionX = 0;
+            float deltaX = 1 / (float)NbColonnes;
+            float deltaY = 1 / (float)NbRangées;
 
-         for (int i = 0; i <= NbColonnes; i++)
-         {
-            float positionY = 1;
-            for (int j = 0; j <= NbRangées; j++)
+            for (int i = 0; i <= NbColonnes; i++)
             {
-               PtsTexture[i, j] = new Vector2(positionX, positionY);
-               positionY -= deltaY;
+                float positionY = 1;
+                for (int j = 0; j <= NbRangées; j++)
+                {
+                    PtsTexture[i, j] = new Vector2(positionX, positionY);
+                    positionY -= deltaY;
+                }
+                positionX += deltaX;
             }
-            positionX += deltaX;
-         }
-      }
+        }
 
-      protected override void InitialiserSommets() // Est appelée par base.Initialize()
-      {
-         int noSommet = -1;
-         for (int j = 0; j < NbRangées; ++j)
-         {
-            for (int i = 0; i <= NbColonnes; ++i)
+        protected override void InitialiserSommets() // Est appelée par base.Initialize()
+        {
+            int noSommet = -1;
+            for (int j = 0; j < NbRangées; ++j)
             {
-               Sommets[++noSommet] = new VertexPositionTexture(PtsSommets[i, j], PtsTexture[i, j]);
-               Sommets[++noSommet] = new VertexPositionTexture(PtsSommets[i, j + 1], PtsTexture[i, j + 1]);
+                for (int i = 0; i <= NbColonnes; ++i)
+                {
+                    Sommets[++noSommet] = new VertexPositionTexture(PtsSommets[i, j], PtsTexture[i, j]);
+                    Sommets[++noSommet] = new VertexPositionTexture(PtsSommets[i, j + 1], PtsTexture[i, j + 1]);
+                }
             }
-         }
-      }
+        }
 
-      public override void Draw(GameTime gameTime)
-      {
-         BlendState blendState = GraphicsDevice.BlendState;
-         GraphicsDevice.BlendState = GestionAlpha;
-         base.Draw(gameTime);
-         GraphicsDevice.BlendState = blendState;
-      }
+        public override void Draw(GameTime gameTime)
+        {
+            BlendState blendState = GraphicsDevice.BlendState;
+            GraphicsDevice.BlendState = GestionAlpha;
+            base.Draw(gameTime);
+            GraphicsDevice.BlendState = blendState;
+        }
 
-      protected override void DessinerTriangleStrip(int noStrip)
-      {
-         GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, Sommets, (NbTrianglesParStrip + 2) * noStrip, NbTrianglesParStrip);
-      }
-   }
+        protected override void DessinerTriangleStrip(int noStrip)
+        {
+            GraphicsDevice.DrawUserPrimitives<VertexPositionTexture>(PrimitiveType.TriangleStrip, Sommets, (NbTrianglesParStrip + 2) * noStrip, NbTrianglesParStrip);
+        }
+    }
 }
