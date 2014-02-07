c14b1b2d5c165096ac56579f3e7fac25c48c2ef0
Trottier95 <trottier.sam@gmail.com>
2014-02-07 12:24:32 -0500

    Vertex Collection + BufferManagement

---
+++ GameOli/Projet Dll/BufferManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Projet_Dll
+{
+    class BufferManager
+    {
+        int Active = 0;
+        VertexBuffer VertexBuffer;
+        IndexBuffer[] IndexBuffers;
+        GraphicsDevice Device;
+
+        BufferManager(VertexPositionNormalTexture[] vertices, GraphicsDevice device)
+        {
+            Device = device;
+
+            VertexBuffer = new VertexBuffer(device, VertexPositionNormalTexture.VertexDeclaration, vertices.Length, BufferUsage.WriteOnly);
+            VertexBuffer.SetData(vertices);
+
+            IndexBuffers = new IndexBuffer[]
+                {
+                    new IndexBuffer(Device, IndexElementSize.ThirtyTwoBits, 100000, BufferUsage.WriteOnly),
+                    new IndexBuffer(Device, IndexElementSize.ThirtyTwoBits, 100000, BufferUsage.WriteOnly)
+                };
+        }
+
+    }
+}
