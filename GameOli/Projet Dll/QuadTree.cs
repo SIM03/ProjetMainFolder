2fc3a57cbd687400234fadc84f8cb0e3dab022df
Aksii <eyouri.0@gmail.com>
2014-02-08 11:47:15 -0400

    Progrès QuadTree

--- GameOli/Projet Dll/QuadTree.cs
+++ GameOli/Projet Dll/QuadTree.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Projet_Dll
 {
     class QuadTree
     {
         QuadNode rootNode { get; set; }
-        VertexCollection Vertices { get; set; }
+        public VertexCollection Vertices { get; private set; }
         BufferManager Buffer { get; set; }
         Vector3 Position { get; set; }
-        int TopNodeSize { get; set; }
+        public int TopNodeSize { get; private set; }
 
         Vector3 CameraPosition { get; set; }
         Vector3 LastCameraPosition { get; set; }
@@ -79,7 +80,7 @@ namespace Projet_Dll
         QuadNode NeighborLeft;
         #endregion
 
-        BoundingBox Bounds { get; set; }
+        BoundingBox Bounds;
 
         NodeType Nodetype { get; set; }
 
@@ -95,7 +96,7 @@ namespace Projet_Dll
 
             AddVertices();
 
-            Bounds = new BoundingBox(ParentTree.Vertices[VertexTopLeft.Index],
+            Bounds = new BoundingBox(ParentTree.Vertices[VertexTopLeft.Index].Position,
                 ParentTree.Vertices[VertexBottomRight.Index].Position);
             Bounds.Min.Y = -950f;
             Bounds.Max.Y = 950f;
@@ -115,15 +116,102 @@ namespace Projet_Dll
             }
         }
 
+        private void AddNeighbors()
+        {
+
+        }
+
+      
         private void AddVertices()
         {
+            // Copy Vertices From The Parent Using NodeType to affect the child vertices to the existing parent vertices
+            switch (Nodetype)
+            {
+                case NodeType.TopLeft:
+                    VertexTopLeft = Parent.VertexTopLeft;
+                    VertexTopRight = Parent.VertexTop;
+                    VertexBottomLeft = Parent.VertexLeft;
+                    VertexBottomRight = Parent.VertexCenter;
+                    break;
+                case NodeType.TopRight:
+                    VertexTopLeft = Parent.VertexTop;
+                    VertexTopRight = Parent.VertexTopRight;
+                    VertexBottomLeft = Parent.VertexCenter;
+                    VertexBottomRight = Parent.VertexRight;
+                    break;
+                case NodeType.BottomLeft:
+                    VertexTopLeft = Parent.VertexLeft;
+                    VertexTopRight = Parent.VertexCenter;
+                    VertexBottomLeft = Parent.VertexBottomLeft;
+                    VertexBottomRight = Parent.VertexBottom;
+                    break;
+                case NodeType.BottomRight:
+                    VertexTopLeft = Parent.VertexCenter;
+                    VertexTopRight = Parent.VertexRight;
+                    VertexBottomLeft = Parent.VertexBottom;
+                    VertexBottomRight = Parent.VertexBottomRight;
+                    break;
+                default:
+                    VertexTopLeft = new QuadNodeVertex { Activated = true, Index = 0 };
+                    VertexTopRight = new QuadNodeVertex { Activated = true, Index = VertexTopLeft.Index + NodeSize };
+                    VertexBottomLeft = new QuadNodeVertex { Activated = true, Index = (ParentTree.TopNodeSize + 1) * ParentTree.TopNodeSize };
+                    VertexBottomRight = new QuadNodeVertex { Activated = true, Index = VertexBottomLeft.Index + NodeSize };
+                    break;
+            }
+            
+            // Adding All Secondary Vertex Of Child
+
+            VertexTop = new QuadNodeVertex 
+            { 
+                Activated = false,
+                Index = VertexTopLeft.Index + (NodeSize / 2) 
+            };
+
+            VertexLeft = new QuadNodeVertex
+            {
+                Activated = false,
+                Index = VertexTopLeft.Index + (ParentTree.TopNodeSize + 1) * (NodeSize / 2)
+            };
+
+            VertexCenter = new QuadNodeVertex
+            {
+                Activated = false,
+                Index = VertexLeft.Index + (NodeSize / 2)
+            };
+            
+            VertexRight = new QuadNodeVertex
+            {
+                Activated = false,
+                Index = VertexLeft.Index + NodeSize
+            };
+
+            VertexBottom = new QuadNodeVertex
+            {
+                Activated = false,
+                Index = VertexBottomLeft.Index + (NodeSize / 2)
+            };
+
+        }
+
+        private void AddChildren()
+        {
+            // Adds All Four Quadrants Child from top left to bot right
+            ChildTopLeft = new QuadNode(NodeType.TopLeft, NodeSize / 2, NodeDepth + 1, this, ParentTree, VertexTopLeft.Index);
+
+            ChildTopRight = new QuadNode(NodeType.TopRight, NodeSize / 2, NodeDepth + 1, this, ParentTree, VertexTop.Index);
+
+            ChildBottomLeft = new QuadNode(NodeType.BottomLeft, NodeSize / 2, NodeDepth + 1, this, ParentTree, VertexLeft.Index);
+
+            ChildBottomRight = new QuadNode(NodeType.BottomRight, NodeSize / 2, NodeDepth + 1, this, ParentTree, VertexCenter.Index);
+
+            HasChildren = true;
 
         }
     }
 
     class QuadNodeVertex
     {
-        int Index { get; set; }
-        bool Activated { get; set; }
+        public int Index { get; set; }
+        public bool Activated { get; set; }
     }
 }
